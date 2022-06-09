2696f4538b43479ae5c2511a391769723d2b52ae
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-10 00:31:15 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// write tyour code here
+var customerName = 'bob'
+const leastFavoriteCustomer = 'some initial value'
+
+function upperCaseCustomerName(bob) {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'billy'
+}
